9e0e71f17bc0570558391de7c6bfaa80a9edff56
SalttySugar <alenwhalker@gmail.com>
2023-12-09 09:25:29 +0200

    make better summary

--- src/components/profile/features/about-me-section/about-me-section.tsx
+++ src/components/profile/features/about-me-section/about-me-section.tsx
@@ -21,23 +21,23 @@ export const AboutMeSection: FC<AboutMeSectionProps> = (props) => {
       <div>
         <div className="text-gray-600 mb-5 space-y-3 flex flex-col relative">
           <p>
-            {t("I am a 24-year-old software engineer with a deep passion for programming and a strong affinity for functional and modern programming languages. My journey in the software development world has been an exciting one, and I am constantly motivated by the opportunities to learn and grow 🚀.")}
+            {t("Hello, I'm a 24-year-old software engineer driven by a genuine love for coding and a preference for contemporary programming languages. My journey in the software development field has been dynamic, fueled by a constant desire to learn and evolve.")}
           </p>
 
           <p>
-            {t("My technical expertise spans a variety of languages, including Haskell, C#, Kotlin, and Scala. Haskell holds a special place in my heart ❤️, and I'm continually fascinated by its elegance and expressiveness. I'm also an enthusiastic learner 📚, currently expanding my horizons by diving into the world of Rust, drawn by its emphasis on safety and performance ⚙️.")}
+            {t("My proficiency extends across various languages like Haskell, C#, Kotlin, and Scala. Haskell, in particular, captivates me with its elegance and expressiveness, holding a special place in my affections. Currently, I'm eagerly delving into Rust, attracted by its emphasis on safety and performance.")}
           </p>
 
           <p className={cn(!isExcerptVisible && "hidden")}>
-            {t("Beyond my professional interests, I lead a balanced life that incorporates creativity and continuous learning 🎸🎹. In my spare time, you can find me strumming my guitar or playing the piano 🎵, as music provides a creative outlet and a source of relaxation. Additionally, I enjoy exploring diverse subjects that extend beyond the realm of software engineering 🌍. This broader perspective not only enriches my problem-solving abilities but also fuels my curiosity 🤓.")}
+            {t("Outside the professional realm, I maintain a well-rounded life that includes creativity and ongoing education. During my free time, you'll find me playing the guitar or piano, using music as both a creative outlet and a means of relaxation. Additionally, I explore diverse subjects beyond software engineering, providing a broader perspective that enhances my problem-solving skills and fuels my curiosity.")}
           </p>
 
           <p className={cn(!isExcerptVisible && "hidden")}>
-            {t("I am on the lookout for opportunities to work and collaborate with like-minded individuals who share my passion for programming and common interests. I am committed to personal and professional growth and am excited about the possibility of contributing my skills and enthusiasm to a team that values innovation, teamwork, and the pursuit of excellence 💪.")}
+            {t("I'm actively seeking opportunities to collaborate with individuals who share my passion for programming and common interests. Committed to personal and professional development, I'm eager to contribute my skills and enthusiasm to a team that values innovation, teamwork, and excellence.")}
           </p>
 
           <p className={cn(!isExcerptVisible && "hidden")}>
-            {t("If you share my enthusiasm for technology and a well-rounded approach to life, I look forward to the opportunity to discuss how I can contribute to your team and goals 🤝.")}
+            {t("If you resonate with my tech enthusiasm and holistic approach to life, I look forward to discussing how I can contribute to your team and objectives.")}
           </p>
 
           <div className={cn(
